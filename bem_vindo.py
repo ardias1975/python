b4e13e148e1aa259dfce9adf28a94f194d5233ac
Andre Dias <ardias2008@gmail.com>
2020-08-03 17:10:29 -0300

    meu primeiro commit

---
+++ bem_vindo.py
@@ -0,0 +1,25 @@
+print("bem vindos ao Python!!")
+print("André", "Ribeiro", "Dias")
+
+pais = "Italia"
+nome: str = "andré"
+idade: str = 45
+salario = 1500.55
+
+print(pais)
+print(salario)
+print(idade + 20)
+
+idade = "joão"
+print(idade)
+print(nome)
+print(type(nome))
+print(idade)
+print(type(idade))
+print(salario)
+print(type(salario))
+
+pais = 25
+print(pais)
+print(type(pais))
+
